ccd92115d287c0549431f9181b9d1403e554a100
George Oluoko <otienog1@gmail.com>
2025-06-20 09:56:00 +0300

    debug filters

--- components/BookingManagementApp.tsx
+++ components/BookingManagementApp.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect, useMemo } from 'react';
-import { format, isBefore, isAfter, isWithinInterval, startOfDay, endOfDay } from 'date-fns';
+import { format, isBefore, isAfter, startOfDay, endOfDay } from 'date-fns';
 import React from 'react';
 import BookingForm from './BookingForm';
 import Modal from './Modal';
